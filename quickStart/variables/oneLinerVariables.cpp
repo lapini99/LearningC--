4883d0ef8c92df3b5134fe86104dc07dbbcdcbf3
alessandro.lapini <alessandro.lapini@conjurer.es>
2024-06-25 16:00:52 +0200

    first commit

---
+++ quickStart/variables/oneLinerVariables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x = 1, y = 2, z = 3;
+    std::cout << x + y + z;
+}
+
+int oneForAll () {
+    int x, y, z;
+    x = y = z = 50;
+    std::cout << x + y + z;
+}
---
+++ quickStart/variables/variables.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    int num = 15;
+    std::cout << num;
+}
